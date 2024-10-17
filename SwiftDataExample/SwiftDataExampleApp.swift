723c044013719c9a5d1e800894c01c0922cd0b24
PashamSrinivasGoud <pashamsrinivasgoud1997@gmail.com>
2024-10-17 12:47:59 +0530

    Initial Commit

---
+++ SwiftDataExample/SwiftDataExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataExampleApp.swift
+//  SwiftDataExample
+//
+//  Created by Pasham Srinivas Goud on 17/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
